86b0ad9eb5cef57d2ccecfb5a91bc4286afdf067
Ekaterina-Lavrinenko <lavrinenko.ey@gmail.com>
2021-06-02 18:21:11 +0100

    Добавляет setTimeout в promise-1.js

--- src/js/promis-1.js
+++ src/js/promis-1.js
@@ -1,6 +1,10 @@
 const delay = ms => {
   // Твой код
-  return new Promise(resolve => resolve(ms));
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve(ms);
+    },ms)
+  );
 };
 
   const logger = time => console.log(`Resolved after ${time}ms`);
